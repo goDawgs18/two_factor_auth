ee9307ba8ef0a670da028a6d3ef56b97c7ab4d17
goDawgs18 <doNotWantToShare@sorry.com>
2019-04-27 17:54:41 -0700

    cleaned up code, added documentation, and commented code

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # two_factor_auth
 In this program I plan on using a RFID key fob and a pin as a two factor authorization process for users to access a secret code
+
+## How to use it
+1. First scan the permitted RFID card
+1. Once you get an Approved card message you will need to enter your pin
+1. Enter your pin
+	1. Psst... the pin is 1234
+1. Once you have entered the correct pin it will grant you access and you can see the secret code
+
+
+## Libraries used
+* MFRC522
+	* This library is used to read the RFID cards
+* SPI
+	* This is used to communicate with the RFID reader
+* Keypad
+	* This library is used to handle the keypad
+* LiquidCrystal
+	* This library controlls the LCD 1602 screen
+
+## Future work
+* Password encryption
+	* Currently the UID and pin used in the program are not encrypted. In the future the inputs should be compared to the encrypted version of these keys
+* More interesting ending
+
+## Code Constants
+* WRONG_PIN_ATTEMPT_LIMIT
+	* This is the number of times a user can get the pin wrong till they have to go back to the start
+* DISPLAY_TRANSITION
+	* This controls how long the display shows the transition statements (EX Card approved, pin approved)
+
+## Connections
+
+RFID-RC522 | Arduino Uno
+------------ | -------------
+3.3V		|		3.3V
+RST			|		9
+GND			|		GND
+IRQ			|		8
+MISO		|		12
+MOSI		|		11
+SCK			|		13
+SDA			|		10
+
+4x4 Pin Pad | Arduino Uno
+------------ | -------------
+R1		|		A5
+R2		|		A4
+R3		|		A3
+R4		|		NOTHING
+C1		|		A2
+C2		|		A1
+C3		|		A0
+C4		|		NOTHING
+
+LCD 1602	|	Arduino Uno
+------------ | -------------
+VSS		|		GND
+VDD		|		5V
+V0		|		10K POT
+RS		|		2
+RW		|		GND
+E		|		3
+D0		|		NOTHING
+D1		|		NOTHING
+D2		|		NOTHING
+D3		|		NOTHING
+D4		|		4
+D5		|		5
+D6		|		6
+D7		|		7
+A		|		3.3V
+K		|		GND
+
+
+## Known Issues
+* The UID from an RFID card can be spoofed
+* Can only read Cards default key
+* Can find the passwords easily in the code
+
--- two_factor_auth/two_factor_auth.ino
+++ two_factor_auth/two_factor_auth.ino
@@ -1,23 +1,15 @@
 /*
- * https://github.com/miguelbalboa/rfid
- * --------------------------------------
- *             MFRC522      Arduino       
- *             Reader/PCD   Uno/101      
- * Signal      Pin          Pin          
- * --------------------------------------
- * RST/Reset   RST          9             
- * SPI SS      SDA(SS)      10           
- * SPI MOSI    MOSI         11
- * SPI MISO    MISO         12 
- * SPI SCK     SCK          13
-*/
-
-// Using to communicate and handle RFID module
+ * Author: Alex Castro
+ */
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Keypad.h>
 #include <LiquidCrystal.h>
 
+/*------------------------------
+ * This is the setup for the LCD module
+ * -----------------------------
+ */
 #define LCD_RS      2
 #define LCD_EN      3
 #define LCD_D4      4
@@ -27,43 +19,63 @@
 
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
+/*------------------------------
+ * END LCD Set up
+ * -----------------------------
+ * -----------------------------
+ * This is the setup for the RFID
+ * -----------------------------
+ */
+
 #define RST_PIN         9           // Configurable, see typical pin layout above
 #define SS_PIN          10          // Configurable, see typical pin layout above
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 
+/*------------------------------
+ * END RFID Set up
+ * -----------------------------
+ * -----------------------------
+ * This is the setup for the Key Pad
+ * -----------------------------
+ */
+
 const byte ROWS = 3; 
 const byte COLS = 3; 
 
-char hexaKeys[ROWS][COLS] = {
+char HEX_KEYS[ROWS][COLS] = {
   {'1', '2', '3'},
   {'4', '5', '6'},
   {'7', '8', '9'}
 };
 
-byte rowPins[ROWS] = {19, 18, 17}; 
-byte colPins[COLS] = {16, 15, 14}; 
+byte ROW_PINS[ROWS] = {19, 18, 17}; 
+byte COL_PINS[COLS] = {16, 15, 14}; 
 
-Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+Keypad customKeypad = Keypad(makeKeymap(HEX_KEYS), ROW_PINS, COL_PINS, ROWS, COLS); 
 
-int matchedRFID;
-
-int matchedPIN;
-
-int state;
-
+/*------------------------------
+ * END Key Pad Set up
+ * -----------------------------
+ * -----------------------------
+ * These are values used in the program
+ * -----------------------------
+ */
+ 
+#define DISPLAY_TRANSITION        1000
 #define WRONG_PIN_ATTEMPT_LIMIT   2
-int pinAttempts;
 
-uint8_t delayTime;
+// This will keep track of where we are in the procedure
+int STATE;
 
+int pinAttempts;  //Keeps track of the # of wrong pin attempts
+char nums[4];     //Holds the pin characters entered
+int keyIdx;       //Keeps track of where we are in the pin sequence
+
+// This will be set by the ISR and let us 
+// know when the RFID module needs to be read
 int checkRFID;
-int timeoutRFID;
 
-char nums[4];
-int keyIdx;
-
-unsigned long prevMillis = 0;
 
 
 void setup() {
@@ -73,11 +85,7 @@ void setup() {
   mfrc522.PCD_Init(); // Init MFRC522 card
 
   lcd.begin(16,2);
-  askCard();
-  
-  // put your setup code here, to run once:
-  //set the digital pins 2-7 to outputs
-//  DDRD = 0b11111100;
+  askForCard();
 
   //making sure that digital pin 8 is an input and others are out
   DDRB &= ~1;
@@ -85,26 +93,23 @@ void setup() {
   //sets all the analog pins for input for pin pad
   DDRC &= 0;
   
-  delayTime = 255;
-  timeoutRFID = 0;
   //setting the first bit PCIE0 on to enable PCINT0
   PCICR |= 1;
 
   //making sure that the Digital pin 8 throws interrupt
   PCMSK0 |= 1;
 
-  matchedRFID = 0;
-  matchedPIN = 0;
   checkRFID = false;
   pinAttempts = 0;
 }
 
-
+//Flags when we need to check the RFID module
 ISR (PCINT0_vect) {
   checkRFID = true;
   Serial.println(F("RFID ISR"));
 }
 
+//Reads the RFID module
 void readRFID() {
   // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
   MFRC522::MIFARE_Key key;
@@ -125,11 +130,9 @@ void readRFID() {
         mfrc522.uid.uidByte[2] == 0xF0 &&
         mfrc522.uid.uidByte[3] == 0x0A) {
     approvedCard();
-    matchedRFID = 1;
-    state = 1;
+    STATE = 1;
   } else {
     wrongCard();
-    matchedRFID = 0;
   }
 }
 
@@ -139,16 +142,14 @@ void checkCode() {
         nums[2] == '3' &&
         nums[3] == '4') {
       approvedPin(); 
-      matchedPIN = true;
-      state = 2;
+      STATE = 2;
   } else {
       pinAttempts++;
       if (pinAttempts >= 2) {
-        state = 3;
+        STATE = 3;
       } else {
         wrongPin();
       }
-      matchedPIN = false;
   }
 }
 
@@ -163,14 +164,14 @@ void numsToSerial() {
   Serial.println();
 }
 
-void askCard() {
+void askForCard() {
   lcd.setCursor(0,0);
   lcd.print("HELLO USER,     ");
   lcd.setCursor(0,1);
   lcd.print("PLEASE SCAN CARD");
 }
 
-void askPin() {
+void askForPin() {
   lcd.setCursor(0,0);
   lcd.print("Now enter PIN   ");
   lcd.setCursor(0,1);
@@ -180,7 +181,6 @@ void askPin() {
   }
   lcd.print(pinStr);
 }
-
 
 void clearScreen() {
   lcd.setCursor(0,0);
@@ -214,7 +214,7 @@ void wrongCard() {
   lcd.print("  RE-SCAN CARD  ");
   myTimer();
   Serial.println("Wrong Card");
-  askCard();
+  askForCard();
 }
 
 void wrongPin() {
@@ -243,53 +243,50 @@ void grantAccess() {
 
 void loop() {
   char customKey = customKeypad.getKey();
-  
+
+  //This will check if the RFID needs to be read
   if (checkRFID) {
+    //Turns off the interrupt trigger
     PCMSK0 &= ~1;
     readRFID();
+    //Turns off the flag to check
     checkRFID = false;
+    //Activates the interrupt again
     PCMSK0 |= 1;
-    //if it found one then it should never run again
   }
   
-  switch (state) {
-    case 1:
-      PCICR &= ~1;
-      askPin();
+  switch (STATE) {
+    case 1: // At this point we have a Card but not the pin
+      PCICR &= ~1; //Turn off the ISR no reason to check
+      askForPin(); 
       if (customKey){
+        //Saved the last key input
         nums[keyIdx] = customKey;
-//        Serial.print(F("Value Pressed: "));
-//        Serial.println(customKey);
-//    
-//        Serial.print(F("keyIdx is currently:"));
-//        Serial.println(keyIdx);
-    
-        numsToSerial();
+        
         if (keyIdx <= 2) {
           keyIdx++;
         } else {
           checkCode();
           keyIdx = 0;
         }
-    
-        Serial.println(customKey);
       }
       break;
     case 2:
-       grantAccess();
-       break;     
+      grantAccess();
+      break;     
     case 3:
       backToStart();
       PCICR |= 1;
-      askCard();
-      state = 0;
-      break;  }
-  
+      askForCard();
+      STATE = 0;
+      break;
+  }
 }
 
 void myTimer() {
+  unsigned long prevMillis = millis();
   unsigned long currMillis = millis();
-  while ((currMillis - prevMillis) < 8000) {
+  while ((currMillis - prevMillis) < DISPLAY_TRANSITION) {
     currMillis = millis();
   }
   prevMillis = currMillis;
